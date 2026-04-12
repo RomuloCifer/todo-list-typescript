7aab29fabcdf9062e803bdd52125646f7d03ce60
Cif <ou_iy@hotmail.com>
2026-04-11 21:41:21 -0300

    feat: adiciona função de exibição de tarefas após submit. Apesar de não estar funcionando :clown_face:

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 
 const form = document.querySelector(".task-form");
 const input = document.querySelector(".task-input");
-const list = document.querySelector(".task-list");
+const list_ = document.querySelector(".task-list");
 
 const tasks =[];
+
+function renderTasks () {
+    list_.innerHTML = "";
+
+    for (const task of tasks) {
+        const item = document.createElement("li");
+        item.textContent = task.title;
+
+        list_.appendChild(item);
+    }
+}
 
 form.addEventListener("submit", function (event) {
     event.preventDefault(); // ñ recarregar a página
@@ -20,6 +31,7 @@ form.addEventListener("submit", function (event) {
     }
     tasks.push(newTask);
     input.value = "";
+    renderTasks();
     console.log(tasks);
 })
 
