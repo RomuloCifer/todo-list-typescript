f911cd21f55389360927bd63eb9ae9e101ee40e8
Cif <ou_iy@hotmail.com>
2026-04-17 10:17:20 -0300

    test(task-logic): adiciona testes de criação, adição, conclusão e remoção

--- tests/task-logic.test.ts
+++ tests/task-logic.test.ts
@@ -1,11 +1,15 @@
+/// <reference types="jest" />
+
 import {
-  createTask,
-  addTask,
-  alternateCompleteTask,
-  removeTask,
-  editTask,
-  saveEditTask,
+    createTask,
+    addTask,
+    alternateCompleteTask,
+    removeTask,
+    editTask,
+    saveEditTask,
 } from "../js/logic/task-logic";
+
+import type { Task } from "../js/types/task";
 
 
 describe("task logic", () => {
@@ -17,4 +21,33 @@ describe("task logic", () => {
         expect(task.editing).toBe(false);
         expect(typeof task.id).toBe("number")
     });
+
+    it("should add a new task in the array", () => {
+        const tasks: Task[]= [];
+        const newTask = createTask("nova tarefa");
+        const result = addTask(tasks, newTask);
+
+        expect(result).toHaveLength(1);
+        expect(result[0].title).toBe("Nova tarefa");
+    });
+
+    it("should alternate the 'completed' status from false to true", () => {
+        const task = createTask("nova task");
+        const tasks = addTask([], task);
+        expect(tasks[0].completed).toBe(false);
+        
+        const updatedTasks = alternateCompleteTask(tasks, task.id);
+        expect(updatedTasks[0].completed).toBe(true);
+        expect(typeof(updatedTasks[0].completed)).toBe("boolean");
+    }
+);
+
+    it("should remove the task given by the ID.", () => {
+        const task = createTask("teste_remove");
+        const tasks = addTask([], task);
+        expect(tasks).toHaveLength(1);
+        const removedTasks = removeTask(tasks, task.id);
+        expect(removedTasks).toHaveLength(0);
+    }
+)
 });
