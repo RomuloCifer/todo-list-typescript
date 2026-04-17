e73cf5986d77d6a02a2e66c98c188a243126f8c7
Cif <ou_iy@hotmail.com>
2026-04-17 10:29:24 -0300

    test(task-logic): reorganiza casos de teste com Arrange/Act/Assert para melhorar legibilidade

--- tests/task-logic.test.ts
+++ tests/task-logic.test.ts
@@ -14,8 +14,13 @@ import type { Task } from "../js/types/task";
 
 describe("task logic", () => {
     it("should create a task with the correct title.", () => {
-        const task = createTask("Entender isso tudo aqui");
+        // Arrange
+        const title = "Entender isso tudo aqui "
 
+        // Act
+        const task = createTask(title);
+
+        // Assert
         expect(task.title).toBe("Entender isso tudo aqui");
         expect(task.completed).toBe(false);
         expect(task.editing).toBe(false);
@@ -23,30 +28,42 @@ describe("task logic", () => {
     });
 
     it("should add a new task in the array", () => {
+        // Arrange
         const tasks: Task[]= [];
         const newTask = createTask("nova tarefa");
+
+        // Act 
         const result = addTask(tasks, newTask);
 
+        // Assert
         expect(result).toHaveLength(1);
         expect(result[0].title).toBe("Nova tarefa");
     });
 
     it("should alternate the 'completed' status from false to true", () => {
+        //Arrange
         const task = createTask("nova task");
         const tasks = addTask([], task);
-        expect(tasks[0].completed).toBe(false);
-        
+
+        // Act
         const updatedTasks = alternateCompleteTask(tasks, task.id);
+
+        // Assert
+        expect(tasks[0].completed).toBe(false);
         expect(updatedTasks[0].completed).toBe(true);
         expect(typeof(updatedTasks[0].completed)).toBe("boolean");
     }
 );
 
     it("should remove the task given by the ID.", () => {
+        // Arrange
         const task = createTask("teste_remove");
         const tasks = addTask([], task);
-        expect(tasks).toHaveLength(1);
+        // Act
         const removedTasks = removeTask(tasks, task.id);
+
+        // Assert
+        expect(tasks).toHaveLength(1);
         expect(removedTasks).toHaveLength(0);
     }
 )
